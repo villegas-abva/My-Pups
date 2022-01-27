337d68937872b283732f0b9840a0ad5e5f00e008
Andrew Villegas <villegas.abva@gmail.com>
2022-01-27 11:03:59 -0600

    Pups Overview: get pups && states

--- lib/bloc/pups_overview/pups_overview_bloc.dart
+++ lib/bloc/pups_overview/pups_overview_bloc.dart
@@ -1,13 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:my_pups/database/models/pup.dart';
+import 'package:my_pups/repository/pups_repository/pups_repository.dart';
 
 part 'pups_overview_event.dart';
 part 'pups_overview_state.dart';
 
 class PupsOverviewBloc extends Bloc<PupsOverviewEvent, PupsOverviewState> {
-  PupsOverviewBloc() : super(PupsOverviewInitial()) {
-    on<PupsOverviewEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final PupsRepository _pupsRepository;
+  PupsOverviewBloc({required PupsRepository pupsRepository})
+      : _pupsRepository = pupsRepository,
+        super(PupsOverviewState()) {
+    on<LoadPups>(_onLoadPups);
+  }
+
+  void _onLoadPups(LoadPups event, Emitter<PupsOverviewState> emit) async {
+    List<Pup> pups = await _pupsRepository.loadPups();
+    emit(state.copyWith(pups: pups, status: PupsOverviewStatus.success));
   }
 }
--- lib/bloc/pups_overview/pups_overview_event.dart
+++ lib/bloc/pups_overview/pups_overview_event.dart
@@ -6,3 +6,5 @@ abstract class PupsOverviewEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class LoadPups extends PupsOverviewEvent {}
