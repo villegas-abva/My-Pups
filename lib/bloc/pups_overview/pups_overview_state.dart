9c264b013e7f8431c1be05306a138c75bc48daa9
Andrew Villegas <villegas.abva@gmail.com>
2022-01-27 08:39:42 -0600

    Pups Overview bloc: add state enum

--- lib/bloc/pups_overview/pups_overview_state.dart
+++ lib/bloc/pups_overview/pups_overview_state.dart
@@ -1,10 +1,28 @@
 part of 'pups_overview_bloc.dart';
 
-abstract class PupsOverviewState extends Equatable {
-  const PupsOverviewState();
+enum PupsOverviewStatus { initial, loading, success, failure }
 
+class PupsOverviewState extends Equatable {
+  const PupsOverviewState({
+    this.status = PupsOverviewStatus.initial,
+    this.pups = const [],
+  });
+
+  final PupsOverviewStatus status;
+  final List<Pup> pups;
+
+// copyWith
+  PupsOverviewState copyWith({
+    PupsOverviewStatus? status,
+    List<Pup>? pups,
+  }) {
+    return PupsOverviewState(
+      status: status ?? this.status,
+      pups: pups ?? this.pups,
+    );
+  }
+
+  // props
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [status, pups];
 }
-
-class PupsOverviewInitial extends PupsOverviewState {}
