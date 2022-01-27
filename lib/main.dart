8307f04c9bf187c7560d87b6aab29763d08d872a
Andrew Villegas <villegas.abva@gmail.com>
2022-01-27 11:03:38 -0600

    Refactor: initialize Firebase in 'main'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_pups/app.dart';
@@ -9,5 +10,7 @@ void main() async {
     blocObserver: MyBlocObserver(),
   );
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(const MyPupsApp());
 }
