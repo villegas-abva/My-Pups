232a905ae659cb69cd0a7e342dedba40b104693f
Andrew Villegas <villegas.abva@gmail.com>
2022-01-27 08:13:01 -0600

    create pups_overview bloc

---
+++ lib/bloc/edit_pup/edit_pup_event.dart
@@ -0,0 +1,8 @@
+part of 'edit_pup_bloc.dart';
+
+abstract class EditPupEvent extends Equatable {
+  const EditPupEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/bloc/edit_pup/edit_pup_state.dart
@@ -0,0 +1,10 @@
+part of 'edit_pup_bloc.dart';
+
+abstract class EditPupState extends Equatable {
+  const EditPupState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class EditPupInitial extends EditPupState {}
