264bb3fd3287ce9d18df500b76643c8c4a659de4
Andrew Villegas <villegas.abva@gmail.com>
2022-01-27 11:03:21 -0600

    Pups Repository: map snapshot to Model

--- lib/repository/pups_repository/pups_repository.dart
+++ lib/repository/pups_repository/pups_repository.dart
@@ -5,13 +5,13 @@ class PupsRepository {
   final CollectionReference _pupsCollection =
       FirebaseFirestore.instance.collection('pups');
 
-  Future<List<Pup>> loadWeightees() async {
+  Future<List<Pup>> loadPups() async {
     QuerySnapshot querySnapshot = await _pupsCollection.get();
     return querySnapshot.docs
         .map(
           (doc) => Pup(
-            name: doc['time'],
-            breed: doc['weight'],
+            name: doc['name'],
+            breed: doc['breed'],
             age: doc['age'],
             isSelected: doc['isSelected'],
           ),
