dab31085600dc04045a0d40832a1acfdecf66562
Andrew Villegas <villegas.abva@gmail.com>
2022-02-03 10:00:43 -0600

    clean: puspect.yaml

--- lib/ui/home/swipeable_screen.dart
+++ lib/ui/home/swipeable_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:slide_to_act/slide_to_act.dart';
 
 class SwipeableScreen extends StatefulWidget {
   const SwipeableScreen({Key? key}) : super(key: key);
