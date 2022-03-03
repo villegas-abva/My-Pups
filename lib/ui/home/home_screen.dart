163960f7a5972dbc01e2c1e39df6b3c6a361c560
Andrew Villegas <villegas.abva@gmail.com>
2022-03-02 19:28:51 -0600

    feat: add bottom snake nav bar

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,19 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:my_pups/bloc/pups/pups_bloc.dart';
-import 'package:my_pups/repository/pups_repository/pups_repository.dart';
-import 'package:my_pups/ui/home/home_view.dart';
+import 'package:flutter_snake_navigationbar/flutter_snake_navigationbar.dart';
+import 'package:my_pups/ui/my_profile/my_profile_screen.dart';
+import 'package:my_pups/ui/my_pups/my_pups_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final screens = [
+    const MyPupsScreen(),
+    const MyProfileScren(),
+  ];
+
+  ShapeBorder? bottomBarShape = const RoundedRectangleBorder(
+      borderRadius: BorderRadius.only(
+    topLeft: Radius.circular(25),
+    topRight: Radius.circular(25),
+  ));
+  SnakeBarBehaviour snakeBarStyle = SnakeBarBehaviour.pinned;
+
+  int _selectedItemPosition = 0;
+  SnakeShape snakeShape = SnakeShape.circle;
+
+  bool showSelectedLabels = false;
+  bool showUnselectedLabels = false;
+
+  Color selectedColor = Colors.black;
+  Color unselectedColor = Colors.blueGrey.withOpacity(0.6);
+
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => PupsBloc(
-        pupsRepository: PupsRepository(),
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      resizeToAvoidBottomInset: true,
+      extendBody: true,
+      body: screens[_selectedItemPosition],
+      bottomNavigationBar: SnakeNavigationBar.color(
+        // height: 80,
+        behaviour: snakeBarStyle,
+        snakeShape: snakeShape,
+        shape: bottomBarShape,
+
+        ///configuration for SnakeNavigationBar
+        snakeViewColor: selectedColor,
+        selectedItemColor:
+            snakeShape == SnakeShape.indicator ? selectedColor : null,
+        unselectedItemColor: unselectedColor,
+
+        showUnselectedLabels: showUnselectedLabels,
+        showSelectedLabels: showSelectedLabels,
+
+        currentIndex: _selectedItemPosition,
+        onTap: (index) => setState(() => _selectedItemPosition = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.pets), label: 'My pups'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        selectedLabelStyle: const TextStyle(fontSize: 14),
+        unselectedLabelStyle: const TextStyle(fontSize: 10),
       ),
-      child: const HomeView(),
     );
+  }
+
+  void _onPageChanged(int page) {
+    switch (page) {
+      case 0:
+        setState(() {});
+        break;
+      case 1:
+        setState(() {});
+        break;
+
+      case 2:
+        setState(() {});
+        break;
+    }
   }
 }
