8fa58fb752891aeae5efd1c3fc1e7704a4c55d8d
Andrew Villegas <villegas.abva@gmail.com>
2022-03-11 10:26:43 -0600

    feat: create skeleton for Add Pup Screen

--- lib/ui/add_pup/add_pup_screen.dart
+++ lib/ui/add_pup/add_pup_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_pups/shared/widgets/custom_app_bar.dart';
+import 'package:my_pups/ui/common/widgets/text/app_regular_text.dart';
 import 'package:my_pups/ui/common/widgets/text_form_field/app_textform_field.dart';
 
 class AddPupScreen extends StatefulWidget {
@@ -33,26 +34,44 @@ class _AddPupScreenState extends State<AddPupScreen> {
           leftIcon: Icons.arrow_back_ios,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: pupInfo.length,
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    AppTextFormField(
-                        hint: pupInfo[index], controller: controller),
-                  ],
-                );
-              },
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: pupInfo.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        AppTextFormField(
+                            hint: pupInfo[index], controller: controller),
+                      ],
+                    );
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Center(
+                    child:
+                        AppRegularText(text: 'Add Pup', color: Colors.white)),
+              ),
+              onPressed: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
