efb7786e2c7a10a9138b463bac89923b1ef6bbab
Andrew Villegas <villegas.abva@gmail.com>
2022-03-11 18:58:11 -0600

    feat: app text form, add properties

--- lib/ui/common/widgets/text_form_field/app_textform_field.dart
+++ lib/ui/common/widgets/text_form_field/app_textform_field.dart
@@ -3,26 +3,41 @@ import 'package:flutter/material.dart';
 class AppTextFormField extends StatelessWidget {
   const AppTextFormField({
     Key? key,
-    required this.hint,
+    required this.label,
     this.hasIcon = false,
     this.icon = Icons.search,
+    this.borderColor = Colors.black,
     required this.controller,
     this.hasNumbers = false,
+    this.hasPrefixIcon = false,
+    this.labelColor = Colors.pinkAccent,
+    this.isPassword = false,
   }) : super(key: key);
-  final String hint;
+  final String label;
   final bool hasIcon;
   final IconData? icon;
   final TextEditingController controller;
   final bool hasNumbers;
+  final bool hasPrefixIcon;
+  final Color borderColor;
+  final Color labelColor;
+  final bool isPassword;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       // keyboardType: hasNumbers ? TextInputType.number : TextInputType.number,
       controller: controller,
+      obscureText: isPassword ? true : false,
       decoration: InputDecoration(
-        // icon: Icon(icon),
-        hintText: hint,
+        prefixIconColor: Colors.green,
+        prefixIcon: hasPrefixIcon ? Icon(icon) : const SizedBox.shrink(),
+        labelText: label,
+        labelStyle: TextStyle(color: labelColor, fontFamily: 'RedHat'),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: borderColor, width: 2.0),
+          borderRadius: BorderRadius.circular(25.0),
+        ),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(20),
         ),
