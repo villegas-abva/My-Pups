05664b5b0ed14115520f3deea4e0786af6de5f30
Andrew Villegas <villegas.abva@gmail.com>
2022-03-22 15:52:59 -0500

    feat: app regular text can be bold

--- lib/ui/common/widgets/text/app_regular_text.dart
+++ lib/ui/common/widgets/text/app_regular_text.dart
@@ -4,18 +4,24 @@ class AppRegularText extends StatelessWidget {
   final double size;
   final String text;
   final Color color;
+  final bool isBold;
   const AppRegularText(
       {Key? key,
       required this.text,
       this.color = Colors.black54,
-      this.size = 16})
+      this.size = 16,
+      this.isBold = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(color: color, fontSize: size, fontFamily: 'RedHat'),
+      style: TextStyle(
+          color: color,
+          fontSize: size,
+          fontFamily: 'RedHat',
+          fontWeight: isBold ? FontWeight.bold : FontWeight.normal),
     );
   }
 }
