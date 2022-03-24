6b75b8aab281971995af610be0eb534fd871745b
Andrew Villegas <villegas.abva@gmail.com>
2022-03-23 19:41:29 -0500

    feat: UI changes to my pups screen

--- lib/ui/screens/my_pups/my_pups_screen.dart
+++ lib/ui/screens/my_pups/my_pups_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_pups/bloc/pups/pups_bloc.dart';
 import 'package:my_pups/database/models/pup/pup.dart';
-
+import 'package:my_pups/ui/common/widgets/clipper/bottom_clipper.dart';
+import 'dart:math' as math;
 import 'package:my_pups/ui/common/widgets/custom_app_bar.dart';
 import 'package:my_pups/shared/widgets/loading_widget.dart';
 import 'package:my_pups/ui/common/widgets/text/app_large_text.dart';
@@ -62,130 +63,131 @@ class MyPupsBody extends StatefulWidget {
 }
 
 class _MyPupsBodyState extends State<MyPupsBody> {
-  final TextEditingController _breedController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      // backgroundColor: Colors.black.withOpacity(0.2),
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(50),
-        child: CustomAppBar(
-            title: 'My Pups',
-            hasRightIcon: true,
-            rightIcon: Icons.add,
-            rightIconTap: () {
-              Navigator.pushNamed(
-                context,
-                '/addPup',
-              );
-            }),
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        backgroundColor: Colors.black87,
+        body: _buildBody(pups: widget.state.pups));
+  }
+}
+
+Widget _buildBody({required List<Pup> pups}) {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        Stack(
+          alignment: Alignment.topCenter,
           children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const SizedBox(height: 20),
-                SizedBox(
-                  height: 280,
-                  width: double.infinity,
-                  child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      itemCount: widget.state.pups.length,
-                      itemBuilder: (context, index) {
-                        var pup = widget.state.pups[index];
-                        return _buildPupCard(context: context, pup: pup);
-                      }),
-                ),
-                const SizedBox(height: 25),
-              ],
+            ClipPath(
+              clipper: BottomClipper(),
+              child: Container(
+                width: double.infinity,
+                height: 180,
+                decoration: BoxDecoration(color: Colors.yellow[800]),
+              ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              padding: const EdgeInsets.all(12),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const AppRegularText(
-                    text: 'Don\'t have a vet yet?',
-                    color: Colors.red,
-                    size: 16,
-                  ),
-                  const AppLargeText(
-                    text: 'Search pet clinics nearby',
-                    size: 20,
-                  ),
-                  const SizedBox(height: 10),
-                  AppTextFormField(
-                    controller: _breedController,
-                    label: 'Enter zip code',
-                  ),
-                  const SizedBox(height: 10),
-                  TextButton(
-                    onPressed: () {},
-                    child: Container(
-                      padding: const EdgeInsets.all(11),
-                      decoration: BoxDecoration(
-                          color: Colors.pinkAccent.withOpacity(0.9),
-                          borderRadius: BorderRadius.circular(20)),
-                      child: const AppRegularText(
-                          text: 'Search', color: Colors.white),
-                    ),
+                  const SizedBox(height: 70),
+                  Row(
+                    children: [
+                      _buildHeader(
+                          title: 'Hi, Andrew',
+                          subtitle: 'Here\'s an overview of your pups')
+                    ],
                   ),
                 ],
               ),
             ),
           ],
         ),
-      ),
-    );
-  }
+        const SizedBox(height: 30),
+        // Text('hula')
+        _buildPups(pups: pups),
+        const SizedBox(height: 30),
+        AnimatedTextWidget(),
+      ],
+    ),
+  );
+}
+
+Widget _buildPups({required List<Pup> pups}) {
+  return SizedBox(
+    height: 580,
+    width: double.infinity,
+    child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemCount: pups.length,
+        itemBuilder: (context, index) {
+          final pup = pups[index];
+          return _buildPupCard(context: context, pup: pup);
+        }),
+  );
 }
 
 Widget _buildPupCard({required BuildContext context, required Pup pup}) {
   return Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+    padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 10),
     child: GestureDetector(
       onTap: () {
         Navigator.pushNamed(context, '/pupDetails', arguments: pup);
       },
-      child: Column(
+      child: Stack(
         children: [
-          Container(
-            height: 200,
+          SizedBox(
+            height: 320,
             width: 200,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(width: 3),
-              borderRadius: BorderRadius.circular(30),
-              image: DecorationImage(
-                image: NetworkImage(
-                  pup.imageUrl,
-                ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                pup.imageUrl,
                 fit: BoxFit.cover,
               ),
             ),
           ),
-          const SizedBox(height: 5),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            width: 200,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: Center(
-              child: AppRegularText(
-                text: pup.name,
-                size: 22,
-                color: Colors.white,
+          // const SizedBox(height: 5),
+          // pup info
+          Positioned(
+            top: 250,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                width: 185,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          AppRegularText(
+                              text: pup.name,
+                              size: 18,
+                              color: Colors.black,
+                              isBold: true),
+                          AppRegularText(
+                            text: pup.breed,
+                            size: 15,
+                          ),
+                        ],
+                      ),
+                      Icon(pup.sex == 'Male' ? Icons.male : Icons.female,
+                          color: pup.sex == 'Male'
+                              ? Colors.blue
+                              : Colors.pinkAccent),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
@@ -193,4 +195,52 @@ Widget _buildPupCard({required BuildContext context, required Pup pup}) {
       ),
     ),
   );
+}
+
+Widget _buildHeader({required String title, required String subtitle}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      AppLargeText(text: title, color: Colors.white, size: 24),
+      AppRegularText(text: subtitle, color: Colors.white),
+    ],
+  );
+}
+
+class AnimatedTextWidget extends StatefulWidget {
+  const AnimatedTextWidget({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedTextWidgetState createState() => _AnimatedTextWidgetState();
+}
+
+class _AnimatedTextWidgetState extends State<AnimatedTextWidget>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: Duration(seconds: 2), vsync: this)
+        ..repeat();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      child: AppRegularText(text: 'hula', color: Colors.white),
+      builder: (BuildContext context, Widget? child) {
+        return Transform.rotate(angle: _controller.value * 0.2 * math.pi);
+      },
+    );
+  }
 }
