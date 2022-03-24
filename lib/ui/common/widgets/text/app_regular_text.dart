dc8fa6009620dfb6ff25b58eb09ba7ee64ba2177
Andrew Villegas <villegas.abva@gmail.com>
2022-03-23 19:39:28 -0500

    feat: add elipsis && bold to app text

--- lib/ui/common/widgets/text/app_regular_text.dart
+++ lib/ui/common/widgets/text/app_regular_text.dart
@@ -5,18 +5,25 @@ class AppRegularText extends StatelessWidget {
   final String text;
   final Color color;
   final bool isBold;
+  final bool hasOverflow;
+  final int maxLines;
+
   const AppRegularText(
       {Key? key,
       required this.text,
       this.color = Colors.black54,
       this.size = 16,
-      this.isBold = false})
+      this.isBold = false,
+      this.hasOverflow = false,
+      this.maxLines = 10})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      maxLines: maxLines,
+      overflow: TextOverflow.ellipsis,
       style: TextStyle(
           color: color,
           fontSize: size,
