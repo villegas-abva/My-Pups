5b7bfde9fa3e9a9201891d0aac26c5eabbf00288
Andrew Villegas <villegas.abva@gmail.com>
2022-03-26 18:56:51 -0500

    feat: add form validation

--- lib/ui/screens/add_pup/add_pup_screen.dart
+++ lib/ui/screens/add_pup/add_pup_screen.dart
@@ -9,10 +9,7 @@ import 'package:my_pups/database/models/pup/pup.dart';
 import 'package:my_pups/ui/common/widgets/button/custom_button.dart';
 import 'package:my_pups/ui/common/widgets/circular_avatar/circular_avatar_widget.dart';
 import 'package:my_pups/ui/common/widgets/clipper/bottom_clipper.dart';
-import 'package:my_pups/ui/common/widgets/custom_app_bar.dart';
-import 'package:my_pups/ui/common/widgets/text/app_large_text.dart';
 import 'package:my_pups/ui/common/widgets/text/app_regular_text.dart';
-import 'package:my_pups/ui/common/widgets/text_form_field/app_textform_field.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 import 'package:my_pups/ui/common/widgets/text_form_field/custom_text_form_field.dart';
 import 'package:path/path.dart';
@@ -31,10 +28,39 @@ class _AddPupScreenState extends State<AddPupScreen> {
   final _formKey = GlobalKey<FormState>();
 
   File? _photo;
-  final ImagePicker _picker = ImagePicker();
 
   firebase_storage.FirebaseStorage storage =
       firebase_storage.FirebaseStorage.instance;
+
+  Future imgFromCamera() async {
+    final ImagePicker _picker = ImagePicker();
+
+    final file = await _picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (file != null) {
+        _photo = File(file.path);
+        // uploadFile();
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future imgFromGallery() async {
+    final ImagePicker _picker = ImagePicker();
+
+    final file = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (file != null) {
+        _photo = File(file.path);
+        // uploadFile();
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
   Future pickImage(ImageSource source) async {
     try {
@@ -50,36 +76,10 @@ class _AddPupScreenState extends State<AddPupScreen> {
     }
   }
 
-  Future imgFromGallery() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      if (pickedFile != null) {
-        _photo = File(pickedFile.path);
-        uploadFile();
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  Future imgFromCamera() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
-
-    setState(() {
-      if (pickedFile != null) {
-        _photo = File(pickedFile.path);
-        uploadFile();
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
   Future uploadFile() async {
     if (_photo == null) return;
     final fileName = basename(_photo!.path);
-    final destination = 'my_pups/';
+    const destination = 'my_pups/';
 
     try {
       final ref = firebase_storage.FirebaseStorage.instance
@@ -117,66 +117,70 @@ class _AddPupScreenState extends State<AddPupScreen> {
       return TextEditingController();
     }).toList();
 
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Stack(
-              alignment: Alignment.topCenter,
-              children: [
-                _buildRoundedContainer(height: 180),
-                _buildAppBar(context: context),
-                _buildImage(topPadding: 110, context: context),
-                _buildFields(
-                    topPadding: 230,
-                    fields: pupFields,
-                    controllers: controllers),
-              ],
-            ),
-            const SizedBox(height: 20),
-            CustomButton(
-              text: 'Save',
-              textColor: Color.fromARGB(255, 70, 90, 121),
-              backgroundColor: Colors.yellow.shade800,
-              onTap: () {
-                if (_formKey.currentState!.validate()) {
-                  // TODO: Edit Pup
-                  try {
-                    final pup = Pup(
-                      name: controllers[0].text,
-                      breed: controllers[1].text,
-                      age: int.parse(controllers[2].text),
-                      owner: controllers[3].text,
-                      petClinic: controllers[4].text,
-                      vetName: controllers[5].text,
-                      vetNotes: controllers[6].text,
-                      lastVisit: controllers[7].text,
-                      nextVisit: controllers[8].text,
-                      sex: dropDownSelection,
-                      imageUrl: imageUrl,
-                      hasClinic: false,
-                      id: '',
-                    );
-                    context.read<PupsBloc>().add(
-                          EditPup(pup: pup),
-                        );
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  _buildRoundedContainer(height: 180),
+                  _buildAppBar(context: context),
+                  _buildImage(topPadding: 110, context: context),
+                  _buildFields(
+                      topPadding: 230,
+                      fields: pupFields,
+                      controllers: controllers),
+                ],
+              ),
+              const SizedBox(height: 20),
+              CustomButton(
+                text: 'Save',
+                textColor: Color.fromARGB(255, 70, 90, 121),
+                backgroundColor: Colors.yellow.shade800,
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    print('hula');
 
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Pup Edited successfully'),
-                      ),
-                    );
-                  } catch (e) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Error adding pup. Please try again.'),
-                      ),
-                    );
+                    // try {
+                    //   final pup = Pup(
+                    //     name: controllers[0].text,
+                    //     breed: controllers[1].text,
+                    //     age: int.parse(controllers[2].text),
+                    //     owner: controllers[3].text,
+                    //     petClinic: controllers[4].text,
+                    //     vetName: controllers[5].text,
+                    //     vetNotes: controllers[6].text,
+                    //     lastVisit: controllers[7].text,
+                    //     nextVisit: controllers[8].text,
+                    //     sex: dropDownSelection,
+                    //     imageUrl: imageUrl,
+                    //     hasClinic: false,
+                    //     id: '',
+                    //   );
+                    //   context.read<PupsBloc>().add(
+                    //         EditPup(pup: pup),
+                    //       );
+
+                    //   ScaffoldMessenger.of(context).showSnackBar(
+                    //     const SnackBar(
+                    //       content: Text('Pup Edited successfully'),
+                    //     ),
+                    //   );
+                    // } catch (e) {
+                    //   ScaffoldMessenger.of(context).showSnackBar(
+                    //     const SnackBar(
+                    //       content: Text('Error adding pup. Please try again.'),
+                    //     ),
+                    //   );
+                    // }
                   }
-                }
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -243,7 +247,6 @@ Widget _buildFields(
               CustomTextFormField(
                 label: fields[index],
                 labelColor: Colors.yellow.shade800,
-                // textColor: Colors.black,
                 controller: controllers[index],
                 hasOnlyNumbers: fields[index] == 'Age' ? true : false,
                 borderColor: Colors.yellow.shade800,
