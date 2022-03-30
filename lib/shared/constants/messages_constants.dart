2ae7ba1ab566adf10b502c135801258846845082
Andrew Villegas <villegas.abva@gmail.com>
2022-03-30 12:25:23 -0500

    feat: add pup incognito link

---
+++ lib/shared/constants/messages_constants.dart
@@ -0,0 +1,4 @@
+class MessagesConstants {
+  static String incognitoPupLink =
+      'https://firebasestorage.googleapis.com/v0/b/my-pups-36a9a.appspot.com/o/my_pups%2Fpup_incognito.jpeg?alt=media&token=55a4d92d-1d1e-4876-addf-419b6806bae9';
+}
